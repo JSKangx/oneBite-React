bc3fa72fcccf84344cb31d6ecabf84317af253d2
Jinsu <injnamek@gmail.com>
2024-11-11 17:19:28 +0900

    React Event 핸들링하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,14 +1,15 @@
-const Button = ({ text, color, children }) => {
+const Button = ({ text, color = 'black', children }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button onClick={onClickButton} style={{ color: color }}>
       {text} - {color.toUpperCase()}
       {children}
     </button>
   );
-};
-
-Button.defaultProps = {
-  color: "black",
 };
 
 export default Button;

f327749582ed9311810a25c2dfa38df7ba18c8b1
Jinsu <injnamek@gmail.com>
2024-11-11 22:43:35 +0900

    비슷한 상태 묶어서 관리하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -8,51 +8,64 @@ import { useState } from 'react';
   4. 자기소개
 */
 const Register = () => {
-  const [name, setName] = useState('이름');
-  const [birth, setBirth] = useState('');
-  const [country, setCountry] = useState('');
-  const [bio, setBio] = useState();
+  const [input, setInput] = useState({ name: '', birth: '', country: '', bio: '' });
+
+  console.log(input);
 
   // 입력값이 변경될 때마다 변경된값을 name 변수에 저장
   const onChangeName = (e) => {
-    setName(e.target.value);
+    setInput({
+      // name이 아닌 다른 속성들을 그대로 복사해와서 유지시키도록 하고
+      ...input,
+      name: e.target.value, // 이 함수에서는 name값만 업데이트
+    });
   };
 
   const onChangeBirth = (e) => {
-    setBirth(e.target.value);
+    setInput({
+      // name이 아닌 다른 속성들을 그대로 복사해와서 유지시키도록 하고
+      ...input,
+      birth: e.target.value, // 이 함수에서는 name값만 업데이트
+    });
   };
 
   const onChangeCountry = (e) => {
-    setCountry(e.target.value);
+    setInput({
+      // name이 아닌 다른 속성들을 그대로 복사해와서 유지시키도록 하고
+      ...input,
+      country: e.target.value, // 이 함수에서는 name값만 업데이트
+    });
   };
 
   const onChangeBio = (e) => {
-    setBio(e.target.value);
+    setInput({
+      // name이 아닌 다른 속성들을 그대로 복사해와서 유지시키도록 하고
+      ...input,
+      bio: e.target.value, // 이 함수에서는 name값만 업데이트
+    });
   };
 
   return (
     <div>
       <div>
-        <input value={name} type='text' onChange={onChangeName} placeholder={'이름'} />
+        <input value={input.name} type='text' onChange={onChangeName} placeholder={'이름'} />
       </div>
 
       <div>
-        <input value={birth} type='date' onChange={onChangeBirth} />
+        <input value={input.birth} type='date' onChange={onChangeBirth} />
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select value={input.country} onChange={onChangeCountry}>
           <option></option>
           <option value='kr'>한국</option>
           <option value='us'>미국</option>
           <option value='uk'>영국</option>
         </select>
-        {country}
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio}></textarea>
-        {bio}
+        <textarea value={input.bio} onChange={onChangeBio}></textarea>
       </div>
     </div>
   );

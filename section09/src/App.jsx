07b73e6ef4cd18129b3da7ede8a4a4fe91cef8b1
Jinsu <injnamek@gmail.com>
2024-11-18 23:13:58 +0900

    refactor: TodoList 토이 프로젝트 - useReducer로 상태관리

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,60 +1,79 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useReducer } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoInput from "./components/TodoInput";
 import Main from "./components/Main";
-import Exam from "./components/Exam";
+
+const DUMMY = [
+  {
+    id: 1,
+    title: "React 공부하기",
+    date: new Date().getTime(),
+    completed: false,
+  },
+  { id: 2, title: "빨래하기", date: new Date().getTime(), completed: false },
+  {
+    id: 3,
+    title: "노래 연습하기",
+    date: new Date().getTime(),
+    completed: false,
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, completed: !item.completed }
+          : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [itemList, dispatch] = useReducer(reducer, DUMMY);
   const idRef = useRef(4);
-  const [itemList, setItemList] = useState([
-    {
-      id: 1,
-      title: "React 공부하기",
-      date: new Date().getTime(),
-      completed: false,
-    },
-    { id: 2, title: "빨래하기", date: new Date().getTime(), completed: false },
-    {
-      id: 3,
-      title: "노래 연습하기",
-      date: new Date().getTime(),
-      completed: false,
-    },
-  ]);
 
   const addItem = (input) => {
-    setItemList((prevList) => [
-      {
-        id: prevList.length > 0 ? idRef.current++ : 1,
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        completed: false,
         title: input,
         date: new Date().getTime(),
-        completed: false,
       },
-      ...prevList,
-    ]);
+    });
   };
 
   // itemList.id 중에 매개변수로 받은 id와 일치하는 item의 completed 속성을 토글
-  const onUpdate = (id) => {
-    setItemList(
-      itemList.map((item) =>
-        item.id === id ? { ...item, completed: !item.completed } : item
-      )
-    );
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
-  const deleteItem = (id) => {
-    setItemList((prevList) => prevList.filter((item) => item.id !== id));
+  const deleteItem = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
 
   return (
     <>
       <div className="max-w-[480px] my-0 mx-auto">
-        {/* <Header />
+        <Header />
         <TodoInput addItem={addItem} />
-        <Main itemList={itemList} deleteItem={deleteItem} onUpdate={onUpdate} /> */}
-        <Exam />
+        <Main itemList={itemList} deleteItem={deleteItem} onUpdate={onUpdate} />
       </div>
     </>
   );

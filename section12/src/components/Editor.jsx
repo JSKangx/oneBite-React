3cbfce277d847437170557e224f89ec3130d0351
Jinsu <injnamek@gmail.com>
2024-11-30 13:40:33 +0900

    feature: Editor 페이지, 일기 데이터 추가 기능 구현

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,9 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+import { useContext, useRef, useState } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const emotionList = [
   {
@@ -26,13 +29,46 @@ const emotionList = [
 ];
 
 const Editor = () => {
-  const emotionId = 5;
+  // Context에서 dispatch 함수 불러오기
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  // 동적 경로 이동
+  const navigate = useNavigate();
+
+  // 날짜 상태 관리
+  const [createdDate, setCreatedDate] = useState("");
+  const onChangeCreatedDate = (e) => {
+    setCreatedDate(e.target.value);
+  };
+
+  // 감정 상태 관리
+  const [emotionId, setEmotionId] = useState(0);
+  const onClickEmotion = (emotionId) => {
+    setEmotionId(emotionId);
+  };
+
+  // 일기 내용 상태 관리
+  const [content, setContent] = useState("");
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // 일기 데이터 생성 및 추가
+  const onSubmitDiary = () => {
+    // 입력 데이터 유효성 검증
+    if (createdDate !== "" && emotionId !== 0 && content !== "") {
+      onCreate(new Date(createdDate).getTime(), emotionId, content);
+      navigate("/");
+    } else {
+      alert("내용을 모두 채워주세요.");
+    }
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input type="date" value={createdDate} onChange={onChangeCreatedDate} />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
@@ -42,17 +78,22 @@ const Editor = () => {
               key={item.emotionId}
               {...item}
               isSelected={item.emotionId === emotionId}
+              onClick={() => onClickEmotion(item.emotionId)}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          placeholder="오늘은 어땠나요?"
+          onChange={onChangeContent}
+          value={content}
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button text={"취소하기"} onClick={() => navigate(-1)} />
+        <Button text={"작성완료"} type={"POSITIVE"} onClick={onSubmitDiary} />
       </section>
     </div>
   );
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,13 +1,16 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
 
 const New = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로가기"} />}
+        leftChild={<Button text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
       />
       <Editor />
     </div>
